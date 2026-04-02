e005d05d0aac17da84fd21ea00bf70f7b4a0f426
auk <auk@littlelights.ai>
2026-04-02 18:55:25 +0800

    Update clawpet-china cask URL

--- Casks/clawpet-china.rb
+++ Casks/clawpet-china.rb
@@ -2,7 +2,7 @@ cask "clawpet-china" do
   version "0.1.0"
   sha256 "c785dcc50755ab6346bc8d28ea5292338a42283b2701ea86ea7e9ec12252f576"
 
-  url "https://github.com/auklittlelights-boop/clawpet/releases/download/v0.1.0/Clawpet%20China_0.1.0_aarch64.dmg",
+  url "https://github.com/auklittlelights-boop/clawpet/releases/download/v0.1.0/Clawpet.China_0.1.0_aarch64.dmg",
       verified: "github.com/auklittlelights-boop/clawpet/"
   name "Clawpet China"
   desc "Clawpet for macOS (China build)"
